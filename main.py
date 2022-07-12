dad80123fccc29ecaa242783864fd569883f631c
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2022-07-12 15:17:34 -0400

    spanish start

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ NUM_CLASSES = 2
 NUM_OUTPUTS = 2
 
 def main():
+    X, y , Xt, yt = get_spanish_data()
     NN = model.Model()
 
     NN.add_layer(nnlayers.LayerDense(2, 64))
@@ -28,7 +29,7 @@ def main():
 
     nnfs.init()
     np.random.seed(0)
-    X, y = nnfsds.spiral_data(NUM_SAMPLES, NUM_CLASSES)
+    # X, y = nnfsds.spiral_data(NUM_SAMPLES, NUM_CLASSES)
 
     NN.train(X, y, NUM_EPOCHS)
 
@@ -39,7 +40,7 @@ def main():
     plt.show()
     plt.cla()
 
-    Xt, yt = nnfsds.spiral_data(NUM_SAMPLES, NUM_CLASSES)
+    # Xt, yt = nnfsds.spiral_data(NUM_SAMPLES, NUM_CLASSES)
     NN.validate(Xt, yt)
 
     plt.scatter(Xt[:, 0], yt, label="TRUE")
@@ -52,6 +53,43 @@ def main():
     # plt.title("LOSS")
     # plt.plot(losses)
     # plt.show()
+
+
+def get_spanish_data():
+    FILE = "Data_col/spanish_nouns.pickle"
+    with open(os.path.join(BASE_DIR, FILE), "rb") as file:
+        raw = pickle.load(file)
+    
+    THES = ["el", "la"]
+    X = []
+    y = []
+    for word in raw:
+        word_split = word.split(" ")
+        if (any(map(word.__contains__, ("el/la", "del ", "al ", "los ", "las ")))) or len(word_split) > 2:
+            continue
+        
+        X.append(word_split[-1][-3:])
+        y.append(THES.index(word_split[0]))
+    
+    X, y = (zip(*sorted(zip(y, X))))
+    
+    nval = int(len(X) * .1)
+    Xt, yt = X[:nval] + X[-nval:], y[:nval] + y[-nval:]
+
+    X = list(X)
+    y = list(y)
+    
+    del X[:nval]
+    del X[-nval:]
+    del y[:nval]
+    del y[-nval:]
+
+    X = np.array(X)
+    y = np.array(y)
+    Xt = np.array(Xt)
+    yt = np.array(yt)
+
+    return (X, y, Xt, yt)
 
 
 if __name__ == "__main__":
